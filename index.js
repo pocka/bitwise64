4283aa584586520208401e3b4ae05d410ab5442e
pocka <pocka.moe@gmail.com>
2016-05-31 23:28:32 +0900

    Refactor

--- index.js
+++ index.js
@@ -1,50 +1,62 @@
-var 
-b64=Math.pow(2,32),
-padding='';
-for(var i=0;i<64;i++){
-	padding+='0';
+/**
+ * Pad string
+ */
+var zeros = '';
+
+for (var i = 0; i < 32; i = (i + 1) | 0) {
+  zeros += '0';
 }
 
-function separate(a){
-	var str=(padding+Number(a).toString(2)).slice(-64);
-	return {
-		hi:parseInt(str.slice(0,32),2),
-		lo:parseInt(str.slice(-32),2)
-	};
+/**
+ * Divides hi and lo
+ */
+function divide(bit) {
+  var bitString = ((zeros + zeros) + (Number(bit).toString(2))).slice(-64);
+
+  return [
+    parseInt(bitString.slice(0, 32), 2), // hi
+    parseInt(bitString.slice(-32), 2) // lo
+  ];
 }
 
-var bitwise64={
-	and:function(a,b){
-		var 
-		left=separate(a),
-		right=separate(b);
-		return parseInt(
-				(padding+(((left.hi&right.hi)>>>0).toString(2))).slice(-32)+
-				(padding+(((left.lo&right.lo)>>>0).toString(2))).slice(-32),2);
-	},
-	or:function(a,b){
-		var 
-		left=separate(a),
-		right=separate(b);
-		return parseInt(
-				(padding+(((left.hi|right.hi)>>>0).toString(2))).slice(-32)+
-				(padding+(((left.lo|right.lo)>>>0).toString(2))).slice(-32),2);
-	},
-	not:function(a){
-		var 
-		left=separate(a);
-		return parseInt(
-				(padding+(((~left.hi)>>>0).toString(2))).slice(-32)+
-				(padding+(((~left.lo)>>>0).toString(2))).slice(-32),2);
-	},
-	xor:function(a,b){
-		var 
-		left=separate(a),
-		right=separate(b);
-		return parseInt(
-				(padding+(((left.hi^right.hi)>>>0).toString(2))).slice(-32)+
-				(padding+(((left.lo^right.lo)>>>0).toString(2))).slice(-32),2);
-	}
+/**
+ * Pads 32bit binary number with 0
+ */
+function pad(bit) {
+  return (zeros + (bit.toString(2))).slice(-32);
+}
+
+/**
+ * Index accessors
+ */
+var Hi = 0;
+var Lo = 1;
+
+
+var b64 = {
+  and: function(a, b) {
+    var _a = divide(a);
+    var _b = divide(b);
+
+    return parseInt(pad((_a[Hi] & _b[Hi]) >>> 0) + pad((_a[Lo] & _b[Lo]) >>> 0), 2);
+  },
+  or: function(a, b) {
+    var _a = divide(a);
+    var _b = divide(b);
+
+    return parseInt(pad((_a[Hi] | _b[Hi]) >>> 0) + pad((_a[Lo] | _b[Lo]) >>> 0), 2);
+  },
+  not: function(a) {
+    var _a = divide(a);
+
+    return parseInt(pad((~_a[Hi]) >>> 0) + pad((~_a[Lo]) >>> 0), 2);
+  },
+  xor: function(a, b) {
+    var _a = divide(a);
+    var _b = divide(b);
+
+    return parseInt(pad((_a[Hi] ^ _b[Hi]) >>> 0) + pad((_a[Lo] ^ _b[Lo]) >>> 0), 2);
+  }
 };
 
-module.exports=bitwise64;
+module.exports = b64;
